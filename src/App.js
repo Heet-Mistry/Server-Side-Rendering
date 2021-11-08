37f6bb86453afad5e07c5142435fe181a8b82e88
Heet <heetmistry@gmail.com>
2021-11-08 23:02:54 +0530

    Added the lazy loading to the details and search params in the home page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import { useState, StrictMode } from "react";
+import React, { useState, StrictMode, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h2>Loading Route</h2>}>
+          <Router>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
